d4cc7e552cb2b88d79b38f623653a75dd47a3d43
li1 <malte@sandstede.com>
2017-11-22 09:52:06 +0100

    add + implement reusable spinner to display chart loading

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { LinearProgress } from 'material-ui/Progress';
+
+function Spinner () {
+  return (
+      <div style={{paddingTop: 12, paddingBottom: 12}}>
+        <div style={{textAlign: "center", color: "#555", marginBottom: 6}}>Lädt...</div>
+        <LinearProgress color="accent" mode="query" />
+      </div>
+  );
+}
+
+export default Spinner;
