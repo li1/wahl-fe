9a87732b802619c7e39016b0b32f4874f1e47983
li1 <malte@sandstede.com>
2017-12-10 00:33:25 +0100

    cleanup voting table

--- src/components/TableExample.js
+++ src/components/TableExample.js
@@ -1,344 +1,128 @@
-import React from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import keycode from 'keycode';
+import React from "react";
+
 import Table, {
-    TableBody,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TablePagination,
-    TableRow,
-    TableSortLabel,
-} from 'material-ui/Table';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Paper from 'material-ui/Paper';
-import Checkbox from 'material-ui/Checkbox';
-import IconButton from 'material-ui/IconButton';
-import Tooltip from 'material-ui/Tooltip';
-import DeleteIcon from 'material-ui-icons/Delete';
-import FilterListIcon from 'material-ui-icons/FilterList';
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from "material-ui/Table";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
+import Paper from "material-ui/Paper";
+import Checkbox from "material-ui/Checkbox";
 
- function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+
+const EnhancedTableHead = props => {
+  const capitalizedHeaders = props.columnData.length > 0 ? 
+    Object.keys(props.columnData[0]).map(title => title.capitalize()) : [];
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox" />
+        {capitalizedHeaders.map((column, idx) => (
+          (column !== "Id") &&
+            <TableCell key={idx}>
+              {column}
+            </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
 }
-
-let counter = 0;
-function createData(jsondata) {
-    for(let i = 0; i <  jsondata.length; i++){
-        counter += 1;
-        jsondata[i]["id"] = counter;
-    }
-    return jsondata;
-}
-
-const columnData = []
-;
-
-class EnhancedTableHead extends React.Component {
-    static propTypes = {
-        numSelected: PropTypes.number.isRequired,
-        onRequestSort: PropTypes.func.isRequired,
-        onSelectAllClick: PropTypes.func.isRequired,
-        order: PropTypes.string.isRequired,
-        orderBy: PropTypes.string.isRequired,
-        rowCount: PropTypes.number.isRequired,
-        columnData : []
-    };
-
-    createSortHandler = property => event => {
-        this.props.onRequestSort(event, property);
-    };
-
-    componentWillReceiveProps (nextProps) {
-        this.setState({
-            columnData : nextProps.columnData
-        });
-    }
-    render() {
-        const { onSelectAllClick, order, orderBy, numSelected, rowCount, columnData } = this.props;
-        let capitalizedHeaders = [];
-        let index = 0;
-        if (columnData.length > 0) {
-            for(var key in Object.keys(columnData[0])){
-                capitalizedHeaders.push(capitalizeFirstLetter(Object.keys(columnData[0])[index++] + "" ));
-            }
-            capitalizedHeaders.pop();
-
-        }
-        let i = 0;
-        return (
-            <TableHead>
-                <TableRow>
-                    <TableCell padding="checkbox">
-                        <Checkbox
-                            indeterminate={numSelected > 0 && numSelected < rowCount}
-                            checked={numSelected === rowCount}
-                            onChange={onSelectAllClick}
-                        />
-                    </TableCell>
-                    {
-                        columnData.map(column => {
-                        return (
-                            <TableCell
-                                key={column.id}
-                                numeric={column.numeric}
-                                padding={column.disablePadding ? 'none' : 'default'}
-                            >
-                                {capitalizedHeaders[i++]
-                                     }
-                            </TableCell>
-
-                        );
-                    }, this)}
-                </TableRow>
-            </TableHead>
-        );
-    }
-}
-
-const toolbarStyles = theme => ({
-    root: {
-        paddingRight: 2,
-    },
-    highlight:
-        theme.palette.type === 'light'
-            ? {
-                color: theme.palette.secondary.A700,
-                backgroundColor: theme.palette.secondary.A100,
-            }
-            : {
-                color: theme.palette.secondary.A100,
-                backgroundColor: theme.palette.secondary.A700,
-            },
-    spacer: {
-        flex: '1 1 100%',
-    },
-    actions: {
-        color: theme.palette.text.secondary,
-    },
-    title: {
-        flex: '0 0 auto',
-    },
-});
-
-let EnhancedTableToolbar = props => {
-    const { numSelected, classes, title } = props;
-
-    return (
-        <Toolbar
-            className={classNames(classes.root, {
-                [classes.highlight]: numSelected > 0,
-            })}
-        >
-            <div className={classes.title}>
-                {numSelected > 0 ? (
-                    <Typography type="subheading">{numSelected} selected</Typography>
-                ) : (
-                    <Typography type="title">{title}</Typography>
-                )}
-            </div>
-            <div className={classes.spacer} />
-            <div className={classes.actions}>
-                {numSelected > 0 ? (
-                    <Tooltip title="Delete">
-                        <IconButton aria-label="Delete">
-                            <DeleteIcon />
-                        </IconButton>
-                    </Tooltip>
-                ) : (
-                    <Tooltip title="Filter list">
-                        <IconButton aria-label="Filter list">
-                            <FilterListIcon />
-                        </IconButton>
-                    </Tooltip>
-                )}
-            </div>
-        </Toolbar>
-    );
-};
-
-EnhancedTableToolbar.propTypes = {
-    classes: PropTypes.object.isRequired,
-    numSelected: PropTypes.number.isRequired,
-};
-
-EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
-
-const styles = theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing.unit * 3,
-    },
-    table: {
-        minWidth: 800,
-    },
-    tableWrapper: {
-        overflowX: 'auto',
-    },
-});
 
 class EnhancedTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: [],
+      data: props.data,
+      title: ""
+    };
+  }
 
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            order: 'asc',
-            selected: [],
-            data: props.data,
-            page: 0,
-            rowsPerPage: 15,
-            title: "default"
-        };
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
     }
 
-    handleRequestSort = (event, property) => {
-        const orderBy = property;
-        let order = 'desc';
+    this.setState({ selected: newSelected });
+  };
 
-        if (this.state.orderBy === property && this.state.order === 'desc') {
-            order = 'asc';
-        }
+  componentWillReceiveProps(nextProps) {
+    nextProps.data.forEach((row, idx) => row["id"] = idx);
+    this.setState({
+      data: nextProps.data,
+      title: nextProps.title,
+      selected: []
+    });
+  }
 
-        const data =
-            order === 'desc'
-                ? this.state.data.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
-                : this.state.data.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1));
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
 
-        this.setState({ data, order, orderBy });
-    };
+  render() {
+    const {
+      data,
+      title
+    } = this.state;
 
-    handleSelectAllClick = (event, checked) => {
-        if (checked) {
-            this.setState({ selected: this.state.data.map(n => n.id) });
-            return;
-        }
-        this.setState({ selected: [] });
-    };
+    return (
+      <Paper>
+        <Toolbar>
+          <Typography type="title">{title}</Typography>
+        </Toolbar>
+        <div>
+          <Table>
+            <EnhancedTableHead columnData={data} />
+            <TableBody>
+              {data
+                .map(n => {
+                  const isSelected = this.isSelected(n.id);
+                  var cells = [];
+                  Object.keys(n).forEach(key => {
+                    if (key !== "id") {
+                      cells.push(<TableCell key={key}>{n[key]}</TableCell>);
+                    }
+                  });
 
-    handleKeyDown = (event, id) => {
-        if (keycode(event) === 'space') {
-            this.handleClick(event, id);
-        }
-    };
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => this.handleClick(event, n.id)}
+                      role="checkbox"
+                      aria-checked={isSelected}
+                      tabIndex={-1}
+                      key={n.id}
+                      selected={isSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox checked={isSelected} />
+                      </TableCell>
 
-    handleClick = (event, id) => {
-        const { selected } = this.state;
-        const selectedIndex = selected.indexOf(id);
-        let newSelected = [];
+                      {cells}
+                    </TableRow>
+                  );
+                })}
 
-        if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, id);
-        } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1));
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1));
-        } else if (selectedIndex > 0) {
-            newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
-            );
-        }
-
-        this.setState({ selected: newSelected });
-    };
-
-    handleChangePage = (event, page) => {
-        this.setState({ page });
-    };
-
-    handleChangeRowsPerPage = event => {
-        this.setState({ rowsPerPage: event.target.value });
-    };
-
-    componentWillReceiveProps (nextProps) {
-       this.setState({
-           data: createData(nextProps.data),
-           title : nextProps.title,
-           rowsPerPage: nextProps.data.length,
-           selected: []
-       });
-    }
-
-    isSelected = id => this.state.selected.indexOf(id) !== -1;
-
-    render() {
-        console.log("TEST" + data)
-        const { classes } = this.props;
-        const { data, order, orderBy, selected, rowsPerPage, page, title } = this.state;
-        const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
-
-        return (
-            <Paper className={classes.root}>
-                <EnhancedTableToolbar numSelected={selected.length} title={title}  />
-                <div className={classes.tableWrapper}>
-                    <Table className={classes.table}>
-                        <EnhancedTableHead
-                            numSelected={selected.length}
-                            order={order}
-                            orderBy={orderBy}
-                            onSelectAllClick={this.handleSelectAllClick}
-                            onRequestSort={this.handleRequestSort}
-                            rowCount={data.length}
-                            columnData={data}
-                        />
-                        <TableBody>
-                            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
-                                const isSelected = this.isSelected(n.id);
-                                var cells = [];
-                                {Object.keys(n).forEach(key => {
-                                    if (key !== "id") {
-                                        cells.push(<TableCell>{n[key]}</TableCell>);
-                                    }
-                                })}
-                                return (
-                                    <TableRow
-                                        hover
-                                        onClick={event => this.handleClick(event, n.id)}
-                                        onKeyDown={event => this.handleKeyDown(event, n.id)}
-                                        role="checkbox"
-                                        aria-checked={isSelected}
-                                        tabIndex={-1}
-                                        key={n.id}
-                                        selected={isSelected}
-                                    >
-                                        <TableCell padding="checkbox">
-                                            <Checkbox checked={isSelected} />
-                                        </TableCell>
-
-                                        {cells}
-
-                                    </TableRow>
-                                );
-                            })}
-                            {emptyRows > 0 && (
-                                <TableRow style={{ height: 49 * emptyRows }}>
-                                    <TableCell colSpan={6} />
-                                </TableRow>
-                            )}
-                        </TableBody>
-                        <TableFooter>
-                            <TableRow>
-                                <TablePagination
-                                    count={data.length}
-                                    rowsPerPage={rowsPerPage}
-                                    page={page}
-                                    onChangePage={this.handleChangePage}
-                                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                                />
-                            </TableRow>
-                        </TableFooter>
-                    </Table>
-                </div>
-            </Paper>
-        );
-    }
+            </TableBody>
+          </Table>
+        </div>
+      </Paper>
+    );
+  }
 }
 
-EnhancedTable.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(EnhancedTable);
+export default EnhancedTable;
--- src/screens/Vote.js
+++ src/screens/Vote.js
@@ -1,71 +1,53 @@
 import React, { Component } from 'react';
-import {
-    Table,
-    TableBody,
-    TableHeader,
-    TableHeaderColumn,
-    TableRow,
-    TableRowColumn,
-} from 'material-ui/Table';
 import Paper from 'material-ui/Paper';
 
 import Grid from 'material-ui/Grid';
-import SortableTable from "../components/SortableTable";
 import Typography from 'material-ui/Typography';
 import EnhancedTable from '../components/TableExample';
 
-const style = {
-    margin: 12,
-};
+export class Vote extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = { tableData: [], tableDataParties: [] };
+  }
+
+  async componentDidMount () {
+    const kandidaten = await fetch("http://localhost:3000/wahlkreisdirektkandidaten/1");
+    const parteien = await fetch("http://localhost:3000/wahlkreisparteien/1");
+    const tableDataParties = await parteien.json();
+    const tableData = await kandidaten.json();
+    this.setState({tableData: tableData, tableDataParties: tableDataParties});
+  }
 
 
-export class Vote extends React.Component {
+  render () {
+    const { tableData, tableDataParties } = this.state;
 
-    constructor (props) {
-        super(props);
-        this.state = { tableData: [], tableDataParties: [] };
-    }
+    return (
+      <div>
+        <Grid container spacing={24}>
+          <Grid item xs={12} md={3}>
+            <Typography type="headline" component="h2">Stimmzettel</Typography>
+            <Typography component="p">
+              Hier können Sie wählen.
+              Sie haben zwei Stimmen. 
+            </Typography>
+          </Grid>
+          <Grid item md={9} />
 
-    async componentDidMount () {
-        const kandidaten = await fetch("http://localhost:3000/wahlkreisdirektkandidaten/1");
-        const parteien = await fetch("http://localhost:3000/wahlkreisparteien/1");
-        const tableDataParties = await parteien.json();
-        const tableData = await kandidaten.json();
-        this.setState({tableData: tableData, tableDataParties: tableDataParties});
-
-    }
-
-
-    render () {
-            const { tableData, tableDataParties } = this.state;
-
-        return (
-        <div>
-            <Grid container spacing={24}>
-                <Grid item xs={12}>
-                    <Paper >Sie haben zwei Stimmen</Paper>
-                </Grid>
-                <Grid item xs={8} width="stretch">
-                    <Paper >
-                        {tableData != undefined &&
-                        <EnhancedTable data={tableData} title="Erststimme"/> }
-                    </Paper>
-                </Grid>
-                <Grid item xs={3} >
-                    <Paper >
-                        <EnhancedTable data={tableDataParties} title="Zweitstimme"/>
-                    </Paper>
-                </Grid>
-
-            </Grid>
-
-
-        </div>
-        );
-
-
-
-    }
+          <Grid item xs={12} md={7}>
+              {tableData !== undefined && <EnhancedTable data={tableData} title="Erststimme"/> }
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <Paper>
+              <EnhancedTable data={tableDataParties} title="Zweitstimme"/>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Vote;
