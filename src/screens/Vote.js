68d4c788b73b636b950ba11b942a53c0e017dcd0
Christoph Anneser <christoph.anneser@tum.de>
2017-12-12 22:41:44 +0100

    coniditional rendering after inserting code (mocked for more efficient testing) for voting

--- src/screens/Vote.js
+++ src/screens/Vote.js
@@ -19,11 +19,15 @@ const style = {
 };
 
 
+
+let wahlkreis = -1;
+
 export class Vote extends React.Component {
 
     constructor (props) {
         super(props);
-        this.state = { tableData: [], tableDataParties: [] };
+        this.state = { tableData: [], tableDataParties: [], wahlkreis : -1, input: "" };
+        this.btnTapped = this.btnTapped.bind(this);
     }
 
     async componentDidMount () {
@@ -31,38 +35,55 @@ export class Vote extends React.Component {
         const parteien = await fetch("http://localhost:3000/wahlkreisparteien/1");
         const tableDataParties = await parteien.json();
         const tableData = await kandidaten.json();
+
         this.setState({tableData: tableData, tableDataParties: tableDataParties});
 
     }
 
+    async btnTapped() {
+        console.log('tapped');
+        const response = await fetch("http://localhost:3000/votingcode/" + "6f7feefa-d717-4f63-9a15-97fa09c67c1f");
+        const res = await response.json();
+        console.log(res);
+        if (res.status === 'OK') {
+            this.setState({wahlkreis: res.WahlkreisID});
+
+        }
+    }
 
     render () {
-            const { tableData, tableDataParties } = this.state;
 
-        return (
-        <div>
-            <Grid container spacing={24}>
-                <Grid item xs={12}>
-                    <Paper >Sie haben zwei Stimmen</Paper>
+        const { tableData, tableDataParties, wahlkreis} = this.state;
+
+           if (wahlkreis === -1) {
+                return (
+                <div>
+                    < input type="text" onChange={this.handleChange}/>
+                    <button onClick={this.btnTapped}>Check Code</button>
+                </div>
+                )
+            }
+            else return (
+                <Grid container spacing={24}>
+                        <Grid item xs={12}>
+                            <Paper >
+                                <h1>Sie wählen im Wahlkreis {wahlkreis} </h1>
+                                <h2>Sie haben 2 Stimmen. Choose wisely.</h2>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={8} width="stretch">
+                            <Paper >
+                                {tableData != undefined &&
+                                <EnhancedTable data={tableData} title="Erststimme"/> }
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={3} >
+                            <Paper >
+                                <EnhancedTable data={tableDataParties} title="Zweitstimme"/>
+                            </Paper>
+                        </Grid>
                 </Grid>
-                <Grid item xs={8} width="stretch">
-                    <Paper >
-                        {tableData != undefined &&
-                        <EnhancedTable data={tableData} title="Erststimme"/> }
-                    </Paper>
-                </Grid>
-                <Grid item xs={3} >
-                    <Paper >
-                        <EnhancedTable data={tableDataParties} title="Zweitstimme"/>
-                    </Paper>
-                </Grid>
-
-            </Grid>
-
-
-        </div>
-        );
-
+           )
 
 
     }
