ee69d9e9c75aae917fbfb235437856b4a8827c58
Christoph Anneser <christoph.anneser@tum.de>
2017-12-13 13:01:24 +0100

    you can vote now

--- src/screens/Vote.js
+++ src/screens/Vote.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Table,
     TableBody,
@@ -19,19 +19,21 @@ const style = {
 };
 
 
-
 let wahlkreis = -1;
 let erststimmenselection = [];
 let zweitstimmenselection = [];
+let code = "";
+
 export class Vote extends React.Component {
 
-    constructor (props) {
+    constructor(props) {
         super(props);
-        this.state = { tableData: [], tableDataParties: [], wahlkreis : -1, input: "" };
+        this.state = {tableData: [], tableDataParties: [], wahlkreis: -1, input: "", voted : false};
         this.btnTapped = this.btnTapped.bind(this);
+        this.sendingVoteToBackend = this.sendingVoteToBackend.bind(this);
     }
 
-    async componentDidMount () {
+    async componentDidMount() {
         const kandidaten = await fetch("http://localhost:3000/wahlkreisdirektkandidaten/1");
         const parteien = await fetch("http://localhost:3000/wahlkreisparteien/1");
         const tableDataParties = await parteien.json();
@@ -42,30 +44,64 @@ export class Vote extends React.Component {
     }
 
     async btnTapped() {
-        console.log('tapped');
-        const response = await fetch("http://localhost:3000/votingcode/" + "6f7feefa-d717-4f63-9a15-97fa09c67c1f");
+        const {code } = this.state;
+        //this.setState({code: "6f7feefa-d717-4f63-9a15-97fa09c67c1f"});
+        //let code =  "6f7feefa-d717-4f63-9a15-97fa09c67c1f";
+        const response = await fetch("http://localhost:3000/votingcode/" + code );
         const res = await response.json();
         console.log(res);
         if (res.status === 'OK') {
             this.setState({wahlkreis: res.WahlkreisID});
-
         }
     }
 
     handleStimmenAuswahl = (title, selection) => {
-       if (title === "Erststimme")
-           erststimmenselection = selection;
-       else zweitstimmenselection = selection;
+        if (title === "Erststimme")
+            erststimmenselection = selection;
+        else zweitstimmenselection = selection;
     }
 
-    render () {
+   async sendingVoteToBackend () {
+        const {code } = this.state;
+        var json = '{ "ErststimmenAuswahl": [' +
+            erststimmenselection.map(kandidat => '"' + kandidat.kandidatid + '"').join(",") + '], "ZweitstimmenAuswahl" : [' +
+            zweitstimmenselection.map(partei => '"' + partei.name + '"').join(",") + '], "code" : "' + code + '" }' ;
+       const voteresult =  await fetch('http://localhost:3000/vote', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: json
+        });
+       console.log(voteresult);
+       const response = await voteresult.json();
+       if (response.status === "OK") {
+           this.setState({voted: true});
+       }
+        alert(response + "test");
+    }
 
-        const {tableData, tableDataParties, wahlkreis} = this.state;
+    handleChange = (votingcode) => {
+       this.setState({code : votingcode.target.value}) ;
+    }
+
+    render() {
+
+        const {tableData, tableDataParties, wahlkreis, voted} = this.state;
+
+        if (voted) {
+            return (
+                <Paper>
+                    <h1>Sie haben Ihre Stimme im Wahlkreis {wahlkreis} erfolgreich abgegeben.</h1>
+                    </Paper>
+            )
+        }
 
         if (wahlkreis === -1) {
             return (
                 <div>
-                    < input type="text" onChange={this.handleChange}/>
+                    <input type="text" onChange={this.handleChange}/>
                     <button onClick={this.btnTapped}>Check Code</button>
                 </div>
             )
@@ -81,17 +117,20 @@ export class Vote extends React.Component {
                 <Grid item xs={8} width="stretch">
                     <Paper>
                         {tableData != undefined &&
-                        <EnhancedTable data={tableData} title="Erststimme" onSelectionChange={this.handleStimmenAuswahl}/>}
+                        <EnhancedTable data={tableData} title="Erststimme"
+                                       onSelectionChange={this.handleStimmenAuswahl}/>}
                     </Paper>
                 </Grid>
                 <Grid item xs={3}>
                     <Paper>
-                        <EnhancedTable data={tableDataParties} title="Zweitstimme" onSelectionChange={this.handleStimmenAuswahl}/>
+                        <EnhancedTable data={tableDataParties} title="Zweitstimme"
+                                       onSelectionChange={this.handleStimmenAuswahl}/>
                     </Paper>
                 </Grid>
-                <button onClick={() => alert (erststimmenselection)}> Wählen!</button>
+                <button onClick={this.sendingVoteToBackend}> Wählen!</button>
             </Grid>
         )
     }
 }
+
 export default Vote;
